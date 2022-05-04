55991bdf4f8a5e28788b928c5e2481b36dfb334d
IfinderCodes <82097226+IfinderCodes@users.noreply.github.com>
2022-05-04 17:17:27 +0000

    Add files via upload

---
+++ IpgeoAPI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace IpgeoAPI
+{
+    class IpgeoAPI
+    {
+        public static IpgeoClass getIPcountry(string ip)
+        {
+            WebClient wc = new WebClient();
+
+            IpgeoClass country = JsonConvert.DeserializeObject<IpgeoClass>(wc.DownloadString($"https://api.iplocation.net/?cmd=ip-country&ip={ip}"));
+
+            return country;
+        }
+    }
+}
---
+++ IpgeoClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpgeoAPI
+{
+    public class IpgeoClass
+    {
+        public string ip { get; set; }
+        public string ip_number { get; set; }
+        public int ip_version { get; set; }
+        public string country_name { get; set; }
+        public string country_code2 { get; set; }
+        public string isp { get; set; }
+        public string response_code { get; set; }
+        public string response_message { get; set; }
+    }
+}
